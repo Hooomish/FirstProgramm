91eab14fdb10b82198f0e60beb75c5f0f58bbc51
Anastasia <coca-cola23-3@mail.ru>
2017-10-28 01:47:36 +0300

    Second commit

--- FirstProgramm/FirstProgramm/Controllers/ValuesController.cs
+++ FirstProgramm/FirstProgramm/Controllers/ValuesController.cs
@@ -4,36 +4,87 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Microsoft.Xrm.Sdk;
+using FirstProgramm.Models;
+using Microsoft.Xrm.Tooling.Connector;
+using Microsoft.Xrm.Sdk.Query;
 
 namespace FirstProgramm.Controllers
 {
+    [RoutePrefix("api")]
     public class ValuesController : ApiController
     {
+        CrmServiceClient service = new CrmServiceClient(
+                "AuthType=AD;" +
+                "Url=https://studdev.scnsoft.com/studdev; " +
+                "Domain=MAIN; " +
+                "Username=crm-test17@scnsoft.com; " +
+                "Password=Abcd1234");
+        string entityName = "scnsoft_securitypaper";
+
+        [Route("read")]
         [HttpGet]
-        public IEnumerable<string> Read()
+        public IEnumerable<Entity> Read()
         {
-            return new string[] { "value1", "value2" };
+            var query = new QueryExpression(entityName) { ColumnSet = new ColumnSet(true) };
+
+            return service.RetrieveMultiple(query).Entities;
         }
 
+        [Route("read/{id}")]
         [HttpGet]
-        public string Read(int id)
+        public Entity Read(string id)
         {
-            return "value";
-        }
+            var SecurityPaper = service.Retrieve(entityName, Guid.Parse(id), new ColumnSet(true));
 
+            if (SecurityPaper != null)
+                return SecurityPaper;
+            else
+                return null;
+        }
+        
+        [Route("create")]
         [HttpPost]
-        public void Create([FromBody]string value)
+        public void Create([FromBody]MyEntity ME)
         {
+            var SecurityPaper = new Entity(entityName);
+            SecurityPaper.Id = Guid.NewGuid();
+            SecurityPaper["scnsoft_name"] = ME.Name;
+            SecurityPaper["scnsoft_description"] = ME.Description;
+            SecurityPaper["scnsoft_product_name"] = ME.ProductName;
+
+            service.Create(SecurityPaper);
         }
 
+        [Route("update/{id}")]
         [HttpPut]
-        public void Update(int id, [FromBody]string value)
+        public void Update(string id, MyEntity ME)
         {
+            var SecurityPaper = service.Retrieve(entityName, Guid.Parse(id), new ColumnSet(true));
+
+            if (SecurityPaper != null)
+            {
+                SecurityPaper["scnsoft_name"] = ME.Name;
+                SecurityPaper["scnsoft_description"] = ME.Description;
+                SecurityPaper["scnsoft_product_name"] = ME.ProductName;
+            }
+
+            service.Update(SecurityPaper);
         }
 
-        // DELETE api/values/5
-        public void Delete(int id)
+        [Route("delete/{id}")]
+        public void Delete(string id)
         {
+            var SecurityPaper = service.Retrieve(entityName, Guid.Parse(id), new ColumnSet(true));
+
+            if(SecurityPaper != null)
+                service.Delete(entityName, Guid.Parse(id));
         }
     }
 }
+
+
+
+
+
+
---
+++ FirstProgramm/FirstProgramm/Models/MyEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Tooling.Connector;
+
+namespace FirstProgramm.Models
+{
+    public class MyEntity
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string ProductName { get; set; }
+
+    }
+}
